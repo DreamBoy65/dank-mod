eb3cba8ce619cc9085b6c314c96576abab7fc612
DreamBoy65 <rajpootpratham904@gmail.com>
2023-05-20 11:16:34 +0530

    up

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ module.exports = {
   "",
   prefix: "$",
   channel: "771243832602787850",
-  dankId: "270904126974590976"
+  dankId: "270904126974590976",
+  data: "storage.json"
 }
--- index.js
+++ index.js
@@ -8,62 +8,133 @@ const config = require("./config.js");
 const client = new Client({
   checkUpdate: false,
 });
-
-let isOkay = true;
+require("@colors/colors");
+const {
+  readFile,
+  writeFile,
+  readdir,
+  lstat,
+  access,
+  constants,
+} = require("fs/promises");
+let data = {
+  commandsUsed: 0,
+  cashEarned: 0,
+  zoo: {},
+};
 
 client.on("ready", async () => {
   console.log(`${client.user.username} is ready!`);
-  console.log("starting auto bot....");
+  console.log("starting auto bot....".blue);
+
+  let Data = await readFile(config.data).catch((e) => null);
+
+  if (!Data) {
+    await writeFile(config.data, JSON.stringify(data));
+  } else if (Data) {
+    data = JSON.parse(Data || data);
+  }
+
+  console.log("Loaded data..".blue);
 
   await setupBot();
 });
 
 client.on("messageCreate", async (msg) => {
-  if (msg.author.id !== config.dankId) return;
-  if (msg.interaction?.user?.id !== client.user.id) return;
-  if (!msg.components.length > 0) return;
-  if (!msg.interaction.commandName.includes("shop sell all")) return;
-  
-  let id = msg.components[0].components[1].customId;
+  if (msg.author.id !== config.dankId && msg.channel.type !== "DM") return;
 
-  await msg.clickButton();
-  console.log("Clicked sell button!");
-  /* if (
-    msg.content.includes("verify") &&
-    msg.content.includes(client.user.username)
+  if (
+    msg.embeds.length > 0 &&
+    msg.embeds[0].description &&
+    msg.embeds[0].description.includes("don't have")
   ) {
-    isOkay = false;
-  }*/
+    if (msg.embeds[0].description.includes("shovel")) {
+      /*
+     await msg.channel.sendSlash(config.dankId, "shop buy shovel");
+      console.log("send buy shovel".yellow);
+      */
+    }
+  }
+
+  if (
+    msg.embeds.length > 0 &&
+    msg.embeds[0].description &&
+    msg.embeds[0].description.includes("⏣") &&
+    !msg.embeds[0].description.includes("sell")
+  ) {
+    let cash = msg.embeds[0].description.split("⏣")[1].trim().match(/\d+/g);
+    console.log(cash);
+
+    data["cashEarned"] += Number(cash.join(""));
+  }
+
+  if (
+    msg.embeds.length > 0 &&
+    msg.embeds[0].description &&
+    msg.embeds[0].description.includes("brought back")
+  ) {
+    let ms = msg.embeds[0].description.split("**")[1].split(">")[1];
+
+    if (!ms) return;
+    ms = ms.replace(/ /g, "_");
+    data["zoo"][msg] += 1;
+  }
+
+  if (
+    msg.components.length > 0 &&
+    msg.interaction?.commandName?.includes("shop sell all")
+  ) {
+    let id = msg.components[0].components[1].customId;
+    await msg.clickButton(id);
+    console.log("Clicked sell button!");
+  }
+
+  await save();
 });
 
 async function setupBot() {
-  let channel = client.channels.cache.get(config.channel);
-  const send = async (msg) => await channel.sendSlash(config.dankId, msg);
-  const log = (msg) => console.log(msg);
+  let channel = await client.users.cache.get(config.dankId).createDM();
+
+  const send = async (msg) =>
+    await channel.sendSlash(config.dankId, msg).catch((e) => null);
+  const log = (msg) => console.log(msg.yellow);
 
   for (let i = 0; i < Infinity; i++) {
-    if (!isOkay) return;
-
     await send("beg");
     log("sent beg.");
+    data["commandsUsed"] += 1;
 
     await sleep(5);
 
     await send("dig");
     log("sent dig.");
+    data["commandsUsed"] += 1;
 
     await sleep(5);
 
     await send("fish");
     log("sent fish.");
+    data["commandsUsed"] += 1;
+
+    await sleep(5);
+
+    await send("hunt");
+    log("sent hunt.");
+    data["commandsUsed"] += 1;
 
     await sleep(5);
 
     await send("shop sell all");
     log("sent sell all");
+    data["commandsUsed"] += 1;
 
+    await save();
     await sleep(30);
   }
+}
+
+async function save() {
+  return await writeFile(config.data, JSON.stringify(data));
 }
 
 function sleep(seconds) {
